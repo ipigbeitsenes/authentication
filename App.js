526c6038b1b0eddd6bb24849bafc0b2706fcbee2
Marco <marconucci1@gmail.com>
2020-11-26 12:42:42 +0100

    files structure

--- App.js
+++ App.js
@@ -2,16 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const arr = (new Array(5)).fill(null)
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {arr.map((value, index) => (
-        <View key={index}>
-          <Text>{index + 1}</Text>
-        </View>
-      ))}
+      
       <StatusBar style="auto" />
     </View>
   );
