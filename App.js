048c9d3b4bf213a77bea98bea63364a9d03d4b06
Lorenzo <lorenzozar@gmail.com>
2020-11-26 13:42:38 +0200

    files structure

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Initialize a new array with 5 elements
+//  .fill(null) fill the elements with null
+// If this is inside the function, it gets initialized every time
+const arr = (new Array(5)).fill(null)
+
 export default function App() {
   return (
     <View style={styles.container}>
