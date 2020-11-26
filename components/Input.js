d39ccb3806efb593b69be550e2c20336afe91496
Alfonso De Lorenzo <alfonso.delorenzo94@gmail.com>
2020-11-26 12:44:13 +0100

    create files structure

---
+++ components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//function component
+const Input = () => {
+    return(
+        <></>
+    )
+}
+
+export default Input;
---
+++ components/Print.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Print = () => {
+    return
+}
+
+export default Print;
