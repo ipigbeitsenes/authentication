35b142bfda59dd635b3e4d0b5a9d5772ae6657b3
ipigbeitsenes <66330932+ipigbeitsenes@users.noreply.github.com>
2020-11-27 11:31:26 +0100

    components

---
+++ components/Input.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {StyleSheet, Text, TextImput, View } from 'react-native'
+
+const Input = ()=>{
+    return(
+        <View style = {} >
+
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    conntainer: {
+        flex: 1,
+        backgroundColor: '#ff'
+
+    }
+})
