7b0617659c8615da55d2a31577af88b25cadec98
Francesco Allegrini <f.allegrini@ideonetwork.it>
2020-11-27 15:02:50 +0100

    commenti

--- components/Input.js
+++ components/Input.js
@@ -1,24 +1,37 @@
 import React, { useRef, useEffect, useState, forwardRef } from 'react'
 import { Animated, StyleSheet, Text, TextInput, View } from 'react-native'
 
+/**
+ * ref non può essere passata come prop
+ * quindi dobbiamo utilizzare `forwardRef`, che la espone
+ * come secondo parametro del nostro componente
+ * https://it.reactjs.org/docs/forwarding-refs.html
+*/
 const Input = forwardRef(({
   containerStyle,
   labelStyle,
   inputStyle,
   isPassword,
   label = 'Placeholder',
-  ...props
+  ...props // props native di TextInput
 }, ref) => {
   const [text, setText] = useState('')
   const [focused, setFocused] = useState(false)
+  /**
+   * creiamo un Animated.Value con valore iniziale 0 che ci
+   * servirà per eseguire l'animazione tramite Animated.timing
+   * e per utilizzare il suo valore dentro il transform delle
+   * Animated.View che abbiamo integrato
+   */
   const animation = useRef(new Animated.Value(0)).current
 
   useEffect(() => {
     Animated.timing(animation, {
       // toValue: focused ? 1 : 0,
+      // toValue: +focused, // stessa cosa che sopra
       toValue: +(focused || (!focused && !!text)), // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
       duration: 300,
-      useNativeDriver: true
+      useNativeDriver: true // mettere `true` solamente se le proprietà che andremo ad animare sono `transform` e `opacity`
     }).start()
   }, [focused])
 
@@ -27,7 +40,7 @@ const Input = forwardRef(({
       <Animated.View
         style={[
           styles.labelContainer,
-          // 
+          // questo metodo è valido ma funziona solo su android
           // {
           //   translateY: animation.interpolate({
           //     inputRange: [0, 1],
@@ -37,8 +50,8 @@ const Input = forwardRef(({
           {
             transform: [{
               translateY: animation.interpolate({
-                inputRange: [0, 1],
-                outputRange: [0, -30]
+                inputRange: [0, 1], // i valori di Animated.Value, gestiti all'interno di useEffect
+                outputRange: [0, -30] // il valore di translate basato sui valori di Animated.Value
               })
             }]
           }
@@ -49,6 +62,7 @@ const Input = forwardRef(({
       <TextInput
         style={[
           styles.input,
+          // possiamo cambiare styles anche in base a delle variabili
           // focused ? styles.inputFocused : undefined,
           // {
           //   borderBottomColor: focused ? '#0f0' : '#000'
@@ -57,7 +71,7 @@ const Input = forwardRef(({
         ]}
         value={text}
         onChangeText={value => setText(value)}
-        // onChangeText={setText}
+        // onChangeText={setText} // stessa cosa che la riga sopra
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
         secureTextEntry={isPassword}
