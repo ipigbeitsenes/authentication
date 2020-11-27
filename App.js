9dbbf6a2a2eb61b62463c03c3d077dd7f3943a24
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-11-27 11:07:11 +0100

    messaggio di stato

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Input from './components/Input'
+import Constants from 'expo-constants'
+import Spacer from './components/Spacer'
+
+const statusBarHeight = Constants.statusBarHeight
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Input />
+        <Spacer size={4} />
+      <Input />
     </View>
   );
 }
@@ -14,8 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
