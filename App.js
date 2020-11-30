77e3868ac0e6b5d6af56e7eaa44960f6d6721fe8
rokko <rocco.caricola89@gmail.com>
2020-11-30 21:50:13 +0100

    file di ieri

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-import React from 'react'
+
+import React, { useRef } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import Input from './components/Input'
 import Spacer from './components/Spacer'
-import Constants from 'expo-constants'
 import ScreenContainer from './components/ScreenContainer'
 import { layoutStyles } from './styles/Layout'
 
-const statusBarHeight = Constants.statusBarHeight
-
 export default function App() {
+  const passwordInput = useRef() // ci serve per fare focus sull'input in cui la applichiamo
+
   return (
     <ScreenContainer style={layoutStyles.container}>
       <StatusBar backgroundColor="transparent" />
 
-      <Spacer size={10} />
-      <Input />
-      {/* <Spacer size={4} />
-      <Input /> */}
+      <ScrollView
+        keyboardShouldPersistTaps="handled" // fa fare blur all'input in focus quando si clicca su altre parti della schermata
+      >
+        <Spacer size={10} />
+        <Input
+          label="Username"
+          onSubmitEditing={() => {
+            passwordInput.current.focus() // quando si fa "invio" sulla tastiera il focus viene spostato all'input successivo
+          }}
+          blurOnSubmit={false} // serve a non far chiudere la tastiera quando si fa focus tramite passwordInput.current.focus()
+        />
+        <Spacer size={4} />
+        <Input label="Password" isPassword ref={passwordInput} />
+      </ScrollView>
+
     </ScreenContainer>
-    // <View style={styles.container}>
-    // </View>
   )
 }
 
