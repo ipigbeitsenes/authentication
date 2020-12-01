36407a2e0f3ebadb3c2efb4d25c7788a2ef85e2f
Lorenzo <lorenzozar@gmail.com>
2020-12-01 11:44:47 +0200

    solve conflict ScreenContainer

--- components/ScreenContainer.js
+++ components/ScreenContainer.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { View, StyleSheet } from 'react-native'
-import sizes from '../config/sizes'
-
-const ScreenContainer = ({ children, style }) => {
-    return (
-        <View
-            style={[styles.container, style]}
-        // {...props}
-        >
-            {children}
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: sizes.statusBarHeight
-    }
-})
-
-
-export default ScreenContainer;
-=======
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import sizes from '../config/sizes'
@@ -38,7 +11,7 @@ const ScreenContainer = ({
   return (
     <View
       style={[styles.container, style]}
-      { ...props }
+      {...props}
     >{children}</View>
   )
 }
@@ -54,4 +27,3 @@ const styles = StyleSheet.create({
 })
 
 export default ScreenContainer
->>>>>>> a80a778f58d5677646a4927de1baccdb6213557b
