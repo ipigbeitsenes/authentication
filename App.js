9ecad1508d6ee7164d97ddbd8b7d3f4fa6012446
Lorenzo <lorenzozar@gmail.com>
2020-12-01 11:39:22 +0200

    ''accepting changes"

--- App.js
+++ App.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Input from './components/Input'
-import Constants from 'expo-constants'
-import ScreenContainer from './components/ScreenContainer'
-import { layoutStyles } from './styles/Layout'
-
-import Spacer from './components/Spacer';
-
-// Initialize a new array with 5 elements
-//  .fill(null) fill the elements with null
-// If this is inside the function, it gets initialized every time
-const arr = (new Array(5)).fill(null)
-
-const StatusBarHeight = Constants.StatusBarHeight
-
-export default function App() {
-  return (
-    <ScreenContainer style={layoutStyles.container}>
-      <StatusBar backgroundColor='red' />
-
-      <View style={styles.container}>
-        <Input />
-        {/* <Spacer size={4} /> */}
-        {/* <Text>First page, life is good</Text> */}
-        {/* <Input /> */}
-        {/* <TouchableOpacity name='Toca chi'>
-          <Text>Toca Chi</Text>
-        </TouchableOpacity> */}
-      </View>
-    </ScreenContainer>
-
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-=======
 // import React, { useRef } from 'react'
 // import { StatusBar } from 'expo-status-bar'
 // import { ScrollView, StyleSheet, Text, View, Button } from 'react-native'
@@ -52,7 +8,7 @@ const styles = StyleSheet.create({
 
 // export default function App() {
 //   const passwordInput = useRef() // ci serve per fare focus sull'input in cui la applichiamo
-  
+
 //   return (
 //     <ScreenContainer style={layoutStyles.container}>
 //       <StatusBar backgroundColor="transparent" />
@@ -94,4 +50,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
->>>>>>> a80a778f58d5677646a4927de1baccdb6213557b
