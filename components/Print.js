aadaea0d7dba355f246c87fdd6d7023542f39bdc
NunzioSofia <nunziosofia.ec@gmail.com>
2020-12-02 18:50:04 +0100

    null

--- components/Print.js
+++ components/Print.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import {View,Text, TextInput} from 'react-native'
 
 const Print=()=>{
-
+    return(
+        <View style= {styles.container}>
+            <Text style= {styles.container}>Label</Text>
+            <TextInput
+            style={styles.input}
+            value ={text}
+            onChangeText={value
+            }
+            />
+        </View>
+    )
 }
+const style =StyleSheet.create({
+    container:{
+        paddingTop:20
+    },
+    input:{
+        height:40
+    },
+    label:{
+        position:'absolute',
+        top:0
+    }
+})
 export default Print;
