77bfb5b688b716662c66cf30357f2a465f97c526
Koli <sokol.muslija@gmail.com>
2020-12-10 12:46:35 +0100

    modifiche form

---
+++ screens/DashBoardScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import ScreenContainer from '../components/ScreenContainer'
+import Title from '../components/Title'
+import Button from '../components/Button'
+import Spacer from '../components/Spacer'
+import { layoutStyles } from '../styles/Layout'
+
+export default function DashBoardScreen(props) {
+  return (
+    <View style={layoutStyles.container}>
+     <Spaceer/>
+     <Spaceer/>
+    </View>
+  )
+}
