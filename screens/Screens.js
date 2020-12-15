b8f6bffeb3914de91d5de62c44d9db5456f34bca
Lorenzo <lorenzozar@gmail.com>
2020-12-15 17:13:33 +0200

    comment

--- screens/Screens.js
+++ screens/Screens.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from '../navigators/AppNavigator'
 import { rootNavigation } from '../Utility/navigation.js'
 import useLoader from "../hooks/useLoader.js"
+
 export default function Screens(){
     const loading = useLoader();
     return (
